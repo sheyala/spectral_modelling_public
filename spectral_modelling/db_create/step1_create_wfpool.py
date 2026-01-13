0eddef98cff264958e46db904659116df9f57bb9
GiuseppeO <giuseppeo824@gmail.com>
2026-01-13 09:59:10 +0100

    step1_create_wfpool.py

--- spectral_modelling/db_create/step1_create_wfpool.py
+++ spectral_modelling/db_create/step1_create_wfpool.py
@@ -116,7 +116,7 @@ def create_wfpool_sac(inpath_wf=cfg.WFPATH, outpath=cfg.WFDBPATH, evdb=cfg.EVDB,
 
             signal = tr.slice(t0_signal, t0_signal + tdura)
             noise = tr.slice(t0_noise, t0_noise + tdura)
-            #TODO add case for which instrumental correction is applied
+            
             for tt in [signal, noise]:
                 tt.detrend('demean')
                 tt.detrend('linear')
@@ -137,23 +137,17 @@ def create_wfpool_sac(inpath_wf=cfg.WFPATH, outpath=cfg.WFDBPATH, evdb=cfg.EVDB,
         pickle.dump(wfpkl, outf, protocol=pickle.HIGHEST_PROTOCOL)
         outf.close()
 
-
-
 def create_wfpool_mseed(inpath_wf=cfg.WFPATH, outpath=cfg.WFDBPATH, 
                         evdb=cfg.EVDB, stadb=cfg.STADB, assocdb=cfg.ASSOCDB, 
                         tdura=cfg.TDURA, removeresp=True, 
                         respfiles=cfg.RESPPATH, verbose=0):
-    """
-    Read processed waveforms for selected events and stations 
-    and store information separately for each event in the form of pickle files 
-    containing Stream() objects with Radial/Transverse components,
-    for both signal and data.
-    Duration of signal and noise windows is currently equal and chosen through
-    the tdura parameter, as (tP-tdura-1, tP-1) for noise and (tS, tS+tdura) 
-    for signal)
-    """
+    
+    def clean_trace_metadata(trace):
+        trace.stats.processing = []
+        trace.stats._format = None
+        trace.stats.response = None
+        return trace
 
-    # parse available associations, events and stations
     assocdb = pd.read_csv(assocdb, sep=";")
     evdb = pd.read_csv(evdb, sep=";")
     stadb = pd.read_csv(stadb, sep=";")
@@ -162,67 +156,67 @@ def create_wfpool_mseed(inpath_wf=cfg.WFPATH, outpath=cfg.WFDBPATH,
     stas = stadb.sta.unique()
 
     pltcount = 0
-    # Loop over available evids
+
     for evid in evids_inst:
-        inwf = inpath_wf + '/' + str(evid) + '/*'
+        # skip if the pkl already exists
+        outfile = os.path.join(outpath, f"{evid}.pkl")          
+        if os.path.isfile(outfile):                             
+            if verbose:                                         
+                print(f"evid_inst {evid}: pkl file already present, skip.")  
+            continue                                           
+       
+
+        inwf = os.path.join(inpath_wf, str(evid), '*')
         try:
             st_evid = read(inwf)
         except:
-            if verbose: 
+            if verbose:
                 print("event with evid_inst ", evid, "not in inpath_wf")
             continue
 
-        #prepare the output paths and items
         if not os.path.exists(outpath):
             os.makedirs(outpath)
-        outpath_plt = outpath + '/plots'
+        outpath_plt = os.path.join(outpath, 'plots')
         if not os.path.exists(outpath_plt):
             os.makedirs(outpath_plt)
 
-        outf = open(outpath + '/' + str(evid) + '.pkl', 'wb')
-        wfpkl = {'signal':{'R':[],'T':[]}, 
-                 'noise':{'R':[],'T':[]}, 
-                 'stacode':[],
-                 'hypodist':[]}
-        st_signal_r = Stream()
-        st_signal_t = Stream()
-        st_noise_r = Stream()
-        st_noise_t = Stream()
+        outf = open(outfile, 'wb')  
+        wfpkl = {'signal': {'R': [], 'T': []},
+                 'noise': {'R': [], 'T': []},
+                 'stacode': [],
+                 'hypodist': []}
 
-        # loop over stations to process traces
+        st_signal_r, st_signal_t = Stream(), Stream()
+        st_noise_r, st_noise_t = Stream(), Stream()
+
         for sta in stas:
-            st_sta = st_evid.copy()
-            st_sta = st_sta.select(station=sta, component='[E|N]')
-            if not st_sta:
-                continue
-            if len(st_sta) != 2:
-                if verbose: 
-                    print('evid_inst ' + str(evid) + ' - station ' + sta +
-                         ' has wrong n. of chans - will be skipped')
+            st_sta_raw = st_evid.select(station=sta, component='[E|N]')
+            if not st_sta_raw or len(st_sta_raw) != 2:
+                if verbose:
+                    print(f"evid_inst {evid} - station {sta} has wrong n. of chans - skipped")
                 continue
 
-            #get association info for current evid_inst and sta from assocdb
-            #if available, otherwise skip station
+            # Clean before deepcopy to avoid MemoryError
+            for tr in st_sta_raw:
+                clean_trace_metadata(tr)
+            st_sta = st_sta_raw.copy()
+
             try:
-                assoc_or = assocdb.loc[assocdb['evid_inst'] == evid]
-                assoc_or_ev = assoc_or.loc[assoc_or['sta'] == sta]
+                assoc_or_ev = assocdb[(assocdb['evid_inst'] == evid) & (assocdb['sta'] == sta)]
                 baz = assoc_or_ev.iloc[0]['baz']
             except:
-                if verbose: 
-                    print('evid_inst ' + str(evid) + ' - station ' + sta +
-                         ' has no association info - will be skipped')
+                if verbose:
+                    print(f"evid_inst {evid} - station {sta} has no association info - skipped")
                 continue
 
-            #get association info for current evid_inst and sta from assocdb
             try:
-                pickP = assoc_or_ev.loc[assoc_or_ev['phase'] == "P"]
-                pickS = assoc_or_ev.loc[assoc_or_ev['phase'] == "S"]
-                t0_noise = UTCDateTime(pickP.iloc[0]['picktime']) - tdura -1
+                pickP = assoc_or_ev[assoc_or_ev['phase'] == "P"]
+                pickS = assoc_or_ev[assoc_or_ev['phase'] == "S"]
+                t0_noise = UTCDateTime(pickP.iloc[0]['picktime']) - tdura - 1
                 t0_signal = UTCDateTime(pickS.iloc[0]['picktime'])
             except:
-                if verbose: 
-                    print('evid_inst ' + str(evid) +
-                          '- P or S pick missing - skipping station ' + sta)
+                if verbose:
+                    print(f"evid_inst {evid} - P or S pick missing - skipping station {sta}")
                 continue
 
             signal = st_sta.slice(t0_signal, t0_signal + tdura)
@@ -234,19 +228,17 @@ def create_wfpool_mseed(inpath_wf=cfg.WFPATH, outpath=cfg.WFDBPATH,
 
             if removeresp:
                 if not respfiles:
-                    if verbose: 
-                        print('no response information found for station ' +
-                               sta + ' - skipping station ')
+                    if verbose:
+                        print(f"no response information for station {sta} - skipped")
                     continue
-                inv = read_inventory(respfiles + '/*')
+                inv = read_inventory(os.path.join(respfiles, '*'))
                 inv = inv.select(station=sta)
                 if len(inv) == 0:
-                    if verbose: 
-                        print('response information for station ' + sta +
-                              'not found - skipping station ')
-                    continue     
-                # print(signal)
-                figname = str(evid) + '_' + st_sta[0].id + '_'
+                    if verbose:
+                        print(f"response info for station {sta} not found - skipped")
+                    continue
+
+                figname = f"{evid}_{st_sta[0].id}_"
                 for tt in signal:
                     tt.remove_response(inventory=inv, pre_filt=cfg.PREFILT,
                                        output='VEL', plot=True)
@@ -261,18 +253,29 @@ def create_wfpool_mseed(inpath_wf=cfg.WFPATH, outpath=cfg.WFDBPATH,
                     plt.close()
                     pltcount+=1
 
+            try:
+                rotated_signal = signal.rotate(method="NE->RT", back_azimuth=baz)
+                rotated_noise = noise.rotate(method="NE->RT", back_azimuth=baz)
 
-            signal_t, signal_r = signal.rotate(method="NE->RT", 
-                                               back_azimuth = baz)
-            noise_t, noise_r = noise.rotate(method="NE->RT", 
-                                            back_azimuth = baz)
+                if len(rotated_signal) != 2 or len(rotated_noise) != 2:
+                    if verbose:
+                        print(f"[WARN] Rotation failed for event {evid}, station {sta}.")
+                    continue
 
-            st_signal_r.append(signal_r)
-            st_noise_r.append(noise_r)
-            st_signal_t.append(signal_t)
-            st_noise_t.append(noise_t)
-            wfpkl['stacode'].append(sta)
-            wfpkl['hypodist'].append(assoc_or_ev.iloc[0]['hypodist'])
+                signal_t, signal_r = rotated_signal
+                noise_t, noise_r = rotated_noise
+
+                st_signal_r.append(signal_r)
+                st_noise_r.append(noise_r)
+                st_signal_t.append(signal_t)
+                st_noise_t.append(noise_t)
+                wfpkl['stacode'].append(sta)
+                wfpkl['hypodist'].append(assoc_or_ev.iloc[0]['hypodist'])
+
+            except Exception as e:
+                if verbose:
+                    print(f"[ERROR] Rotation error for event {evid}, station {sta}: {e}")
+                continue
 
         wfpkl['signal']['R'] = st_signal_r
         wfpkl['signal']['T'] = st_signal_t
